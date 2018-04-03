5630059aedcaaa3643c62604bb929ce64c6b00f5
suspecie <sspecie13@gmail.com>
2018-04-03 14:52:22 -0300

    Criação dos componentes e troca do favicon

--- app2/src/app/app.module.ts
+++ app2/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    HomeComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
