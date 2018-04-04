00e883376085d9c78956057713aea0315ce58cbc
suspecie <sspecie13@gmail.com>
2018-04-04 12:44:45 -0300

    Criando Services, Model e utilizando ngFor

--- app2/src/app/home/home.component.ts
+++ app2/src/app/home/home.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  providers: [ OfertasService ]
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
+
+  public ofertas: Oferta[];
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    console.log(this.ofertasService.getOfertas());
+    this.ofertas = this.ofertasService.getOfertas();
   }
 
 }
